a8bace7635c63b395cbcf89b73abd2650e8f6b48
PatinhoGamer <40546524+PatinhoGamer@users.noreply.github.com>
2019-05-23 16:40:06 +0100

    yeet

---
+++ app/src/main/java/com/bringitlist/bringit/App.java
@@ -0,0 +1,13 @@
+package com.bringitlist.bringit;
+
+import android.app.Application;
+//esta classe serve para poder ter objetos nas várias atividades
+//é criado um objeto desta classe quando a aplicação é aberta
+//para o pegarmos faz-se:
+//App app = (App) getApplication();
+//App- nome da classe
+//app- nome da variavel
+public class App extends Application {
+
+
+}
