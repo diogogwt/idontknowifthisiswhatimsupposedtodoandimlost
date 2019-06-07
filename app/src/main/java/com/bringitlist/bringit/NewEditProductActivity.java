6726e6759d9cf0bd995ad42c13df59a9dbe2715c
PatinhoGamer <40546524+PatinhoGamer@users.noreply.github.com>
2019-06-07 10:03:36 +0100

    ja tem a cena de editar e adicionar produto e categoria

--- app/src/main/java/com/bringitlist/bringit/NewEditProductActivity.java
+++ app/src/main/java/com/bringitlist/bringit/NewEditProductActivity.java
@@ -1,27 +1,51 @@
 package com.bringitlist.bringit;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.bringitlist.bringit.Database.DBNames;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class NewEditProductActivity extends AppCompatActivity {
 
+    public static String TAG = "NewEditProductActivity";
+
     private App app;
-    private int prod_id;
+    private Integer prod_id;
+    private Integer cat_id;
+    private String imageName;
     private boolean newProd = false;
     private Spinner spinner;
-    private EditText name;
-    private EditText price;
+    private EditText nameView;
+    private EditText priceView;
+    private ImageView imageView;
+    private String curPhotoPath;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     @Override
@@ -30,44 +54,124 @@ public class NewEditProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_edit_product);
 
         app = (App) getApplication();
-        SQLiteDatabase db = app.getReadableDB();
+        final SQLiteDatabase db = app.getReadableDB();
 
         Intent intent = getIntent();
         prod_id = intent.getIntExtra("prod_id", -1);
 
         spinner = findViewById(R.id.new_edit_product_spinner);
-        name = findViewById(R.id.new_edit_product_name);
-        price = findViewById(R.id.new_edit_product_price);
+        nameView = findViewById(R.id.new_edit_product_name);
+        priceView = findViewById(R.id.new_edit_product_price);
+        imageView = findViewById(R.id.new_edit_product_image);
 
         if (prod_id != -1) {
             newProd = false;
-            Cursor cursor = db.rawQuery("select cat_id,name,price,image from products where id=?", new String[]{String.valueOf(prod_id)});
+            Cursor cursor = db.rawQuery("select cat_id,name,price,image from products where id=?", new String[]{prod_id.toString()});
             cursor.moveToFirst();
-
-            name.setText(cursor.getString(1));
-            price.setText(cursor.getString(2));
-
+            cat_id = cursor.getInt(0);
+            nameView.setText(cursor.getString(1));
+            priceView.setText(cursor.getString(2));
+            imageName = cursor.getString(3);
+            App.loadImageToView(this, imageName, imageView);
             cursor.close();
         } else {
             newProd = true;
             Cursor cursor = db.rawQuery("select max(id)+1 as id from products;", null);
             cursor.moveToFirst();
+            cat_id = 0;
             prod_id = cursor.getInt(0);
             cursor.close();
         }
 
+        fillCatSpinner();
+    }
 
-        Cursor cursor = db.rawQuery("select name from categories order by id", null);
+    public void pickFromCamera(View view) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    && checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
-        String[] array = new String[cursor.getCount()];
-
-        for (int i = 0; cursor.moveToNext(); i++) {
-            array[i] = cursor.getString(0);
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 11);
+                return;
+            }
         }
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+        /*Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, array);
+                Log.e(TAG, "IOException", e);
+            }
+            if (photoFile != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        }*/
+    }
 
-        spinner.setAdapter(arrayAdapter);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 11) {
+            if (grantResults.length > 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                pickFromCamera(null);
+            }
+        }
+    }
+
+   /* private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        curPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                bitmap = Bitmap.createScaledBitmap(bitmap, 400, 400, false);
+                imageName = prod_id + ".jpg";
+
+                FileOutputStream outputStream = openFileOutput(imageName, MODE_PRIVATE);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
+                outputStream.close();
+
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+                /*if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.parse(curPhotoPath));
+                imageView.setImageBitmap(bitmap);
+
+                bitmap = Bitmap.createScaledBitmap(bitmap, 400, 400, false);
+
+                FileOutputStream outputStream = openFileOutput(prod_id + ".jpg", MODE_PRIVATE);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream);
+                outputStream.close();
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }*/
     }
 
     public void onClickAddCategory(View view) {
@@ -81,30 +185,117 @@ public class NewEditProductActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         SQLiteDatabase db = app.getWritableDB();
-
                         String[] args = new String[]{editText.getText().toString()};
-
                         db.execSQL("insert into categories(name) values(?)", args);
-
+                        fillCatSpinner();
                         dialog.dismiss();
                     }
                 }).create().show();
     }
 
+
     @Override
-    protected void onStop() {
-        super.onStop();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_prod_menu, menu);
+        return true;
+    }
 
-        SQLiteDatabase db = app.getWritableDB();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_prod_menu_delete: {
 
-        if (newProd) {
-            String[] args = new String[]{String.valueOf(prod_id), null, name.getText().toString(), price.getText().toString(), null};
+                new AlertDialog.Builder(this)
+                        .setTitle("Are you sure you want to remove this item?")
+                        .setNegativeButton(getString(R.string.cancel), null)
+                        .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
 
-            db.execSQL(DBNames.INSERT_PRODUCT, args);
-        } else {
-            String[] args = new String[]{null, name.getText().toString(), price.getText().toString(), null, String.valueOf(prod_id)};
+                                File file = new File(getFilesDir(), imageName);
+                                if (file.exists())
+                                    file.delete();
 
-            db.execSQL("update products set cat_id=?,name=?,price=?,image=? where id=?", args);
+                                if (!newProd) {
+                                    app.getWritableDB().execSQL("delete from products where id=?", new Integer[]{prod_id});
+                                }
+                                finish();
+                            }
+                        })
+                        .create()
+                        .show();
+
+                break;
+            }
+            case R.id.new_prod_menu_save: {
+
+                String name = nameView.getText().toString();
+                String price = priceView.getText().toString();
+
+                if (name.equals("")) {
+                    Toast.makeText(this, getString(R.string.insert_prod_name), Toast.LENGTH_LONG).show();
+                    return true;
+                }
+                if (price.equals("")) {
+                    priceView.setText("0");
+                    price = "0";
+                }
+
+                SQLiteDatabase db = app.getWritableDB();
+
+                if (newProd) {
+                    String[] args = new String[]{prod_id.toString(), cat_id.toString(), name, price, imageName};
+                    db.execSQL(DBNames.INSERT_PRODUCT, args);
+                    finish();
+                } else {
+                    String[] args = new String[]{cat_id.toString(), name, price, imageName, prod_id.toString()};
+                    db.execSQL("update products set cat_id=?,name=?,price=?,image=? where id=?", args);
+                    finish();
+                }
+                break;
+            }
         }
+        return true;
+    }
+
+    public void fillCatSpinner() {
+        Cursor cursor = app.getReadableDB().rawQuery("select id,name from categories order by id", null);
+
+        final IdName[] cats = new IdName[cursor.getCount()];
+        String[] array = new String[cursor.getCount()];
+        int spinnerSelection = 0;
+        for (int i = 0; cursor.moveToNext(); i++) {
+            array[i] = cursor.getString(1);
+
+            IdName temp = new IdName();
+            cats[i] = temp;
+            temp.id = cursor.getInt(0);
+            temp.name = array[i];
+
+            if (cat_id == temp.id) {
+                spinnerSelection = i;
+            }
+        }
+        cursor.close();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, array);
+
+        spinner.setAdapter(arrayAdapter);
+        spinner.setSelection(spinnerSelection);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                cat_id = cats[position].id;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    class IdName {
+        int id = -1;
+        String name = null;
     }
 }
